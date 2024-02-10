bdbb94b5781f332721689f6f04c61af98c6d802b
krok-code <maks.smolinskyi@gmail.com>
2024-02-10 11:10:33 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
+import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './components/theme';
